b40cebdf4b11e3cf25be05165522ab9f02d9e717
MohammadYehya <mohammad7446@gmail.com>
2025-02-11 18:34:05 +0500

    Update page.tsx

--- src/app/Skills/page.tsx
+++ src/app/Skills/page.tsx
@@ -15,56 +15,55 @@ export default function Skills() {
     <div className="h-screen">
       <Navbar />
       <ScrollArea className="h-[calc(100%-45px)]">
-
-      <div
-        className={`my-10 w-full flex justify-center transition-all duration-1000 ${
-          open ? "opacity-100" : "opacity-0"
-        }`}
-      >
-        <div className="flex px-4 md:w-[80%] justify-center flex-col">
-          <h1 className="text-7xl font-bold flex justify-center mb-5">
-            Skills
-          </h1>
-          {MySkills.content.map((item, index) => (
-            <div className="" key={index}>
-              <div className="flex justify-center items-center mt-10">
-                <div className="w-[50px] border flex"></div>
-                <div className="flex text-sm px-4 text-gray-400">
-                  {item.group}
+        <div
+          className={`my-10 w-full flex justify-center transition-all duration-1000 ${
+            open ? "opacity-100" : "opacity-0"
+          }`}
+        >
+          <div className="flex px-4 md:w-[80%] justify-center flex-col">
+            <h1 className="text-7xl font-bold flex justify-center mb-5">
+              Skills
+            </h1>
+            {MySkills.content.map((item, index) => (
+              <div className="" key={index}>
+                <div className="flex justify-center items-center mt-10">
+                  <div className="w-[50px] border flex"></div>
+                  <div className="flex text-sm px-4 text-gray-400">
+                    {item.group}
+                  </div>
+                  <div className="border flex flex-1"></div>
                 </div>
-                <div className="border flex flex-1"></div>
-              </div>
-              <div className="flex flex-wrap justify-center gap-y-3 md:gap-x-3 mt-4">
-                {item.content.map((details, index) => {
-                  return (
-                    <Card
-                      key={index}
-                      className={`flex flex-row justify-start lg:flex-1 w-full md:min-w-[49%] md:max-w-[49%] lg:min-w-[30%] lg:max-w-[30%] p- h-14 hover:h-32 hover:cursor-pointer group items-center transition-all duration-300 ease-in-out overflow-clip`}
-                    >
-                      <div className="font-bold items-start flex w-full pl-5">
-                        {details.name}
-                      </div>
-                      <Image
-                        src={details.imgpath}
-                        alt=""
-                        width={175}
-                        height={1}
-                        className="opacity-60"
-                        draggable={false}
-                      />
-                      {/* <div
+                <div className="flex flex-wrap justify-center gap-y-3 md:gap-x-3 mt-4">
+                  {item.content.map((details, index) => {
+                    return (
+                      <Card
+                        key={index}
+                        className={`flex flex-row justify-start lg:flex-1 w-full md:min-w-[49%] md:max-w-[49%] lg:min-w-[30%] lg:max-w-[30%] p- h-14 hover:h-32 hover:cursor-pointer group items-center transition-all duration-300 ease-in-out overflow-clip`}
+                      >
+                        <div className="font-bold font-mono items-start flex w-full pl-5">
+                          {details.name}
+                        </div>
+                        <Image
+                          src={details.imgpath}
+                          alt=""
+                          width={175}
+                          height={1}
+                          className="opacity-80"
+                          draggable={false}
+                        />
+                        {/* <div
                       className={`group-hover:flex text-transparent group-hover:text-black transition-all duration-500 ease-in-out`}
                     >
                       Proficiency: {details.proficiency}
                     </div> */}
-                    </Card>
-                  );
-                })}
+                      </Card>
+                    );
+                  })}
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
-      </div>
       </ScrollArea>
     </div>
   );
