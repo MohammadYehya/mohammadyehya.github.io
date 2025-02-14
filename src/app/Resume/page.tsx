5e24f0a5de552030b6aec60199a402b81a7d90e5
MohammadYehya <mohammad7446@gmail.com>
2025-02-14 18:22:44 +0500

    Update page.tsx

--- src/app/Resume/page.tsx
+++ src/app/Resume/page.tsx
@@ -1,17 +1,25 @@
-'use client'
+"use client";
 import { useEffect, useState } from "react";
 import Navbar from "@/components/myui/Navbar";
 
 export default function Resume() {
   const [open, setOpen] = useState(false);
-      useEffect(() => {
-        setOpen(true);
-      },[]);
+  useEffect(() => {
+    setOpen(true);
+  }, []);
   return (
     <>
       <Navbar />
-      <div className={`my-10 flex w-full justify-center transition-all duration-1000 ${open ? 'opacity-100' : 'opacity-0'}`}>
-        <h1 className="text-7xl font-bold">Resume</h1>
+      <div
+        className={`my-10 flex flex-col w-full justify-center items-center transition-all duration-1000 ${
+          open ? "opacity-100" : "opacity-0"
+        }`}
+      >
+        <h1 className="text-7xl font-bold mb-8">Resume</h1>
+        <iframe
+          src="/data/MohammadYehyaHayati_Resumev2.01.pdf"
+          className="w-[70%] h-screen border shadow-black shadow-2xl"
+        ></iframe>
       </div>
     </>
   );
