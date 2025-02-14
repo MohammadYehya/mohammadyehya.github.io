d3efc41e1180cd2ad0dbc7490ce644326031763d
MohammadYehya <mohammad7446@gmail.com>
2025-02-14 18:48:42 +0500

    Update page.tsx

--- src/app/Resume/page.tsx
+++ src/app/Resume/page.tsx
@@ -2,6 +2,7 @@
 import { useEffect, useState } from "react";
 import Navbar from "@/components/myui/Navbar";
 import { MyResume } from "../../../public/data/config";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 export default function Resume() {
   const [open, setOpen] = useState(false);
@@ -9,19 +10,21 @@ export default function Resume() {
     setOpen(true);
   }, []);
   return (
-    <>
+    <div className="h-screen">
       <Navbar />
-      <div
-        className={`my-10 flex flex-col w-full justify-center items-center transition-all duration-1000 ${
-          open ? "opacity-100" : "opacity-0"
-        }`}
-      >
-        <h1 className="text-7xl font-bold mb-8">Resume</h1>
-        <iframe
-          src={MyResume.resumePath}
-          className="w-[70%] h-screen border shadow-black shadow-2xl"
-        ></iframe>
-      </div>
-    </>
+      <ScrollArea className="h-[calc(100%-45px)]">
+        <div
+          className={`my-10 flex flex-col w-full justify-center items-center transition-all duration-1000 ${
+            open ? "opacity-100" : "opacity-0"
+          }`}
+        >
+          <h1 className="text-7xl font-bold mb-8">Resume</h1>
+          <iframe
+            src={MyResume.resumePath}
+            className="w-[70%] h-screen border shadow-black shadow-2xl"
+          ></iframe>
+        </div>
+      </ScrollArea>
+    </div>
   );
 }
