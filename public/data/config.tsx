56d9f9a659470672f0521c23e49fc4767a7f3313
Mohammad Yehya <56927485+MohammadYehya@users.noreply.github.com>
2026-02-19 02:20:14 +0500

    Update resume path to version 3.01

--- public/data/config.tsx
+++ public/data/config.tsx
@@ -831,7 +831,7 @@ export const MyResume = {
   name: "Resume",
   icon: <FileText className="scale-[60%]" />,
   href: "/Resume",
-  resumePath: "/data/MohammadYehyaHayati_Resume_v2_05.pdf",
+  resumePath: "/data/MohammadYehyaHayati_Resume_v3_01.pdf",
 };
 export const NavItems = {
   MySkills,
